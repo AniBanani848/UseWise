b527d3f404e25d67d4eadd46a66d65b711c06f7b
unknown <anna.watzin@gmail.com>
2026-03-26 11:32:40 +0200

    views

--- UseWISE/UseWISE/views.py
+++ UseWISE/UseWISE/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
-def items_placeholder(request):
-    return render(request, "items_placeholder.html")
-
