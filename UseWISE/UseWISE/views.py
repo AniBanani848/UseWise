6502163a8731d45d6eebdb7887de4d836e230c7a
unknown <anna.watzin@gmail.com>
2026-03-26 11:31:42 +0200

    mainWISE

--- UseWISE/UseWISE/views.py
+++ UseWISE/UseWISE/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
+def items_placeholder(request):
+    return render(request, "items_placeholder.html")
+
